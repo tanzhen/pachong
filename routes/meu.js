fc9e530f3e1be6ce77d34ff2c2d3a96030de12dc
tanzhen <nicminika@gmail.com>
2015-08-09 21:06:29 +0800

    1

--- routes/meu.js
+++ routes/meu.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('meu/index', { title: 'ee' });
+  res.render('meu/index', { title: '资源搜索' });
 });
 
 module.exports = router;
